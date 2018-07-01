5372adfa60520be03c82e6808a807ac2f4ee6914
mkarthikn <mkarth3@gmail.com>
2018-07-01 00:19:58 -0400

    add a new code with unordered_map

--- LeetCode_twosum.cpp
+++ LeetCode_twosum.cpp
@@ -1,3 +1,4 @@
+//96ms performance
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -8,6 +9,27 @@ public:
             if(it != mapElements.end())
             {
                 return vector<int>({ it->second, i});
+            }
+            else
+            {
+                mapElements[nums[i]] = i;
+            }
+        }
+        return vector<int>({ 0, 0});
+    }
+};
+
+//2ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mapElements;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            unordered_map<int, int>::iterator it = mapElements.find(target-nums[i]);
+            if(it != mapElements.end())
+            {
+                return vector<int>({ mapElements[target-nums[i]], i});
             }
             else
             {
